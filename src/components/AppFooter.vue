8348a71d322509c558f274d89bc7d8a229f5a078
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-01 18:02:56 +0100

    added footer layout

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,6 +4,19 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <footer>
+        <div class="container">
+            <div class="upper-footer">
+                <nav></nav>
+                <figure></figure>
+            </div>
+            <div class="bottom-footer">
+                <div class="actions"></div>
+                <div class="follow"></div>
+            </div>
+        </div>
+    </footer>
+</template>
 
 <style scoped></style>
