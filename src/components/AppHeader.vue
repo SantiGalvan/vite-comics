c200a1eb0dbe4bed77743e7e6e8bf8e709f6897f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-01 15:54:06 +0100

    added header layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,63 +2,84 @@
 export default {
     name: 'AppHeader',
     data: () => ({
-    links: [
-        {
-            text: 'Characters',
-            url: '#',
-            current: false,
-        },
-        {
-            text: 'Comics',
-            url: '#',
-            current: true,
-        },
-        {
-            text: 'Movies',
-            url: '#',
-            current: false,
-        },
-        {
-            text: 'TV',
-            url: '#',
-            current: false,
-        },
-        {
-            text: 'Games',
-            url: '#',
-            current: false,
-        },
-        {
-            text: 'Collectibles',
-            url: '#',
-            current: false,
-        },
-        {
-            text: 'Videos',
-            url: '#',
-            current: false,
-        },
-        {
-            text: 'Fans',
-            url: '#',
-            current: false,
-        },
-        {
-            text: 'News',
-            url: '#',
-            current: false,
-        },
-        {
-            text: 'Shop',
-            url: '#',
-            current: false,
-        },
-    ]
+        links: [
+            {
+                text: 'Characters',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Comics',
+                url: '#',
+                current: true,
+            },
+            {
+                text: 'Movies',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'TV',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Games',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Collectibles',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Videos',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Fans',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'News',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Shop',
+                url: '#',
+                current: false,
+            },
+        ]
 
-})
+    })
 };
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container header-elements">
+            <figure>
+            </figure>
+            <nav>
+            </nav>
+        </div>
+    </header>
+</template>
 
-<style></style>
+<style scoped>
+header {
+    background-color: goldenrod;
+}
+
+.header-elements {
+    height: 150px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
