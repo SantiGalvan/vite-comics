ed13289f4fc05fcebe985575360fc8e8a15f32a9
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-02 16:47:34 +0100

    added upper footer contents

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,13 +7,57 @@ export default {
 <template>
     <footer>
         <div class="upper-footer">
-            <div class="container">
-                <nav></nav>
-                <figure></figure>
+            <div class="container footer-container">
+                <nav>
+                    <div class="col">
+                        <h4>Dc comics</h4>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <h4>Shop</h4>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h4>Dc</h4>
+                        <ul>
+                            <li><a href="#">Therms of use</a></li>
+                            <li><a href="#">Privacy policy (NEW)</a></li>
+                            <li><a href="#">Add Choise</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certification</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h4>Dc comics</h4>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="logo-bg"></div>
             </div>
         </div>
         <div class="bottom-footer">
-            <div class="container">
+            <div class="container footer-container">
                 <div class="actions"></div>
                 <div class="follow"></div>
             </div>
@@ -21,4 +65,53 @@ export default {
     </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.upper-footer {
+    background-image: url(../assets/img/footer-bg.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.bottom-footer {
+    min-height: 100px;
+    background-color: #303030;
+}
+
+.footer-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+}
+
+nav {
+    display: flex;
+    padding-top: 20px;
+    gap: 20px
+}
+
+li a {
+    color: gray;
+    font-size: 0.8rem;
+}
+
+.col {
+    flex-basis: calc(100% / 3);
+    margin-bottom: 50px;
+}
+
+h4 {
+    color: #fff;
+    text-transform: uppercase;
+    margin: 20px 0;
+}
+
+.logo-bg {
+    background-image: url(../assets/img/dc-logo-bg.png);
+    background-repeat: no-repeat;
+    background-size: 500px;
+    background-position: center;
+
+    flex-basis: 50%;
+    align-self: stretch;
+}
+</style>
