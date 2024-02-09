73f69375fc5df465515f932defbd2768a02d800f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-09 18:49:40 +0100

    added element & style

--- src/components/DcCard.vue
+++ src/components/DcCard.vue
@@ -1,9 +1,39 @@
 <script>
 export default {
-    name: 'AppContent'
+    name: 'AppContent',
+    props: {
+        image: String,
+        title: String
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <div class="dc-card">
+        <figure>
+            <img :src="image" :alt="title">
+        </figure>
+        <h3>{{ title }}</h3>
+    </div>
+</template>
 
-<style></style>
+<style scoped>
+.dc-card {
+    padding: 10px;
+}
+
+figure {
+    height: 280px;
+    overflow: hidden;
+}
+
+img {
+    display: block;
+    height: 100%;
+}
+
+h3 {
+    color: #fff;
+    margin-top: 10px;
+}
+</style>
